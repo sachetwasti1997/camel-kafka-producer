42fc9713291f99cf9b60d266a283fbb574accbbf
sachetwasti1997 <sachetwasti61@gmail.com>
2024-08-21 11:05:44 +0530

    Made Kafka Consumer made routes and put a login processor

--- src/main/java/com/sachet/camel_microservice/routes/SendKafkaMessage.java
+++ src/main/java/com/sachet/camel_microservice/routes/SendKafkaMessage.java
@@ -1,5 +1,6 @@
 package com.sachet.camel_microservice.routes;
 
+import com.sachet.camel_microservice.utility.LoggingProcessor;
 import com.sachet.camel_microservice.utility.RandomDateUtility;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
@@ -10,9 +11,11 @@ import java.time.LocalDateTime;
 public class SendKafkaMessage extends RouteBuilder {
 
     private final RandomDateUtility randomDateUtility;
+    private final LoggingProcessor processor;
 
-    public SendKafkaMessage(RandomDateUtility randomDateUtility) {
+    public SendKafkaMessage(RandomDateUtility randomDateUtility, LoggingProcessor processor) {
         this.randomDateUtility = randomDateUtility;
+        this.processor = processor;
     }
 
     @Override
@@ -20,6 +23,7 @@ public class SendKafkaMessage extends RouteBuilder {
         from("timer://test-kafka?period=10000")
 //                .setBody(simple("This message is being sent at the time "+ LocalDateTime.now()))
                 .bean(randomDateUtility, "nextDate")
+                .process(processor)
                 .to("kafka:camel-topic");
     }
 }
--- src/main/java/com/sachet/camel_microservice/utility/LoggingProcessor.java
+++ src/main/java/com/sachet/camel_microservice/utility/LoggingProcessor.java
@@ -1,15 +1,25 @@
 package com.sachet.camel_microservice.utility;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 import org.apache.camel.Processor;
+import org.apache.camel.component.kafka.KafkaConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
 public class LoggingProcessor implements Processor {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingProcessor.class);
 
     @Override
     public void process(Exchange exchange) throws Exception {
-        LOGGER.info("The message Received is from Processor: {}", exchange.getMessage().getBody());
+        Message message = exchange.getIn();
+        LOGGER.info("Message Received: {}, partition: {}, topic-name: {}, offset: {}",
+                    message.getBody(),
+                    message.getHeader(KafkaConstants.PARTITION),
+                    message.getHeader(KafkaConstants.TOPIC),
+                    message.getHeader(KafkaConstants.OFFSET)
+                );
     }
 }
